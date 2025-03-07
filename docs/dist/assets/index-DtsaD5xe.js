0ae562471b65f6e5ce05d8b8d51db2bcdd35434f
prabhsharan36 <prabhsharan369@gmail.com>
2025-03-08 02:34:04 +0530

    feat: home page

---
+++ docs/dist/assets/index-DtsaD5xe.js
@@ -0,0 +1,14 @@
+import{j as e,L as r,c as l,u as o}from"./index-CF3i_2bS.js";var d="vocs_Button_button",c="vocs_Button_button_accent";function h({children:t,className:n,href:a,variant:s}){return e.jsx(r,{className:l(n,d,s==="accent"&&c),href:a,variant:"styleless",children:t})}const u={downloadResumeUrl:"https://drive.google.com/uc?export=download&id=1wq5PvQi5owyzg8e8RiCp35xA-3wzT06l"},g={layout:"landing"};function i(t){const n={a:"a",img:"img",li:"li",p:"p",ul:"ul",...o(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.p,{children:e.jsx(n.img,{src:"https://ik.imagekit.io/qu20t6lqgz/IMG_20230825_102544258~3%20(1).jpg?updatedAt=1741377929032",alt:"Prabhsharan Singh Profile",title:"Prabhsharan Singh Profile"})}),`
+`,e.jsx(n.p,{children:"Hi, I’m Prabhsharan."}),`
+`,e.jsx(n.p,{children:"I’m a Software Engineer passionate about building impactful solutions and learning through the process."}),`
+`,e.jsx(n.p,{children:"I discovered my love for programing during my undergraduate years and developed a passion for startups and building products during my first internship. I began with frontend development, transitioned to fullstack, and now focus primarily on backend development. Currently, I’m a Founding Engineer at Jugyah,  India’s fastest growing technology enabled residential brokerage."}),`
+`,e.jsx(n.p,{children:"Over the years, I’ve had the privilege of working with early-stage startups and collaborating closely with founders. This experience has shaped my product-focused approach to development, enabling me to:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Consider the broader product-wide impacts of features."}),`
+`,e.jsx(n.li,{children:"Design solutions that create net-positive outcomes for all stakeholders."}),`
+`,e.jsx(n.li,{children:"Anticipate and plan for feature scalability and extensibility."}),`
+`,e.jsx(n.li,{children:"Keep my fellow developers and mentees excited about the work we’re doing."}),`
+`]}),`
+`,e.jsx(n.p,{children:"When I’m not coding, I’m usually learning about the philosophies of the greatest minds and practicing self-control. I’m also deeply interested in personal finance, health, and music. On the side, I’m a noob barista, experimenting with brewing the perfect cup of coffee. You can often find me obsessing over something new and fascinating."}),`
+`,e.jsxs(n.p,{children:["You can connect with me on ",e.jsx(n.a,{href:"https://www.linkedin.com/in/prabhsharan-singh-sandhu/",children:"LinkedIn"}),"."]}),`
+`,e.jsx(h,{href:u.downloadResumeUrl,children:"Download Resume"})]})}function m(t={}){const{wrapper:n}={...o(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{m as default,g as frontmatter};
